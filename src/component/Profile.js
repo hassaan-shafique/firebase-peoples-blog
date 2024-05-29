eec1284fd0f40dbeb6f2e17d488679572bb0bc34
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-29 14:39:07 +0500

    profile

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -1,17 +1,32 @@
 import React from 'react'
+const welcome = "Hello";
 
 const Profile = () => {
   return (
     <>
-      <div className='profile'> 
-          
-          <h1> This is my profile page 
-          </h1>
- 
-        
+      <div className="profile">
+        <h1> {welcome.user} </h1>
+
+        <form>
+          <label>First Name</label>
+          <input type="text" placeholder='First Name' />
+         <label>Last Name</label>
+          <input type="text" placeholder='Last Name' />
+          <label>Bio</label>
+          <input type='text'/>
+          <label>Email:</label>
+          <input type="email"  placeholder='Write Your Email'/>
+          <label>Projects</label>
+          <label>Title</label>
+
+          <input type='text'/>
+          <label>Description</label>
+         <textarea/>
+         <button type='submit'> Save</button>
+        </form>
       </div>
     </>
-  )
+  );
 }
 
 export default Profile
