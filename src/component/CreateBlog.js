16c8f5b43fa1e254a33f81884017fc6c99114357
hassaan-shafique <hassaanshafique398@gmail.com>
2024-05-30 13:16:04 +0500

    create post

--- src/component/CreateBlog.js
+++ src/component/CreateBlog.js
@@ -3,11 +3,17 @@ import React from 'react'
 const CreateBlog = () => {
   return (
     <>
-      <h1> Create Your Blog Here
-      
-      </h1>
+      <div className="main-page">
+        <div className="blog-main">
+          <h1>Write Your Blog</h1>
+          <label>Title</label>
+          <input placeholder="Your Blog Title" type="text" />
+          <label>Upload Your Image</label>
+          <input type="file" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default CreateBlog
