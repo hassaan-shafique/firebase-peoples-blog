0f639bbb8456e902270c43403f18a40ff3692f52
hassaan-shafique <hassaanshafique398@gmail.com>
2024-06-08 23:58:16 +0500

    add

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -117,7 +117,8 @@ const Profile = () => {
       <h1> Name : {formData.name}</h1>
       <h1> Bio: {formData.bio}</h1>
       </div>
-      <div> <h1>HEllo this is firbase</h1></div>
+     
+     
     </>
   );
 }
