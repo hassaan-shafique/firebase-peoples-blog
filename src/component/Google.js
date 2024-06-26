8aa70c328160658a2cc27670dc74c742502c23cb
hassaan-shafique <hassaanshafique398@gmail.com>
2024-06-26 17:01:34 +0500

    google auth

--- src/component/Google.js
+++ src/component/Google.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {auth,provider} from './Firebase'
+import { signInWithPopup } from 'firebase/auth';
+import { useState } from 'react';
+import Home from './Home'
 
 
 
 const Google = () => {
+  const [value ,setValue] = useState('');
+  const handleClick= () =>{
+    signInWithPopup(auth,provider) .then ((data) =>{
+   setValue(data.user.email)
+   localStorage.setItem ("email", data.user.email)
+    })
+  }
+
+  useEffect (()=>{
+    setValue(localStorage.getItem('email'))
+  })
   return (
     <div>
-      <button className="google-btn">
+    {value? <Home/>: 
+      <button onClick={handleClick}
+      className="google-btn">
         <img src="/google.svg" width={25} height={25} alt="google img" />
         Continue with Google
       </button>
+    }
     </div>
   );
 }
