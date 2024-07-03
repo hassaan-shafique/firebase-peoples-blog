c3691b1d0159d7265a8c2ebb1684ea3638797486
hassaan-shafique <hassaanshafique398@gmail.com>
2024-07-03 17:23:35 +0500

    quill

--- src/component/CreateBlog.js
+++ src/component/CreateBlog.js
@@ -1,13 +1,37 @@
 import React, { useState } from 'react'
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
+//import "quill/dist/quill.core.css";
 
 const CreateBlog = () => {
   const [value, setValue] = useState ('');
-  var toolbarOptions = [['bold' ,'italic'], ['link', 'image']];
-const module = {
-  toolbar: toolbarOptions,
-};
+ const toolbarOptions = [
+   ["bold", "italic", "underline", "strike"], // toggled buttons
+   ["blockquote", "code-block"],
+   ["link", "image", "video", "formula"],
+
+   [{ header: 1 }, { header: 2 }], // custom button values
+   [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+   [{ script: "sub" }, { script: "super" }], // superscript/subscript
+   [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+   [{ direction: "rtl" }], // text direction
+
+   [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+   [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+   [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+   [{ font: [] }],
+   [{ align: [] }],
+
+   ["clean"], // remove formatting button
+ ];
+
+ 
+   const modules=[
+    {
+     toolbar: toolbarOptions,
+   },
+  ];
 
   return (
     <>
@@ -18,11 +42,14 @@ const module = {
           <input placeholder="Your Blog Title" type="text" />
           <label>Upload Your Image</label>
           <input type="file" />
-
-          <ReactQuill module={module} theme="snow" value={value} onChange= {setValue} />
         </div>
       </div>
-
+      <ReactQuill
+        module={modules}
+        theme="snow"
+        value={value}
+        onChange={setValue}
+      />
     </>
   );
 }
