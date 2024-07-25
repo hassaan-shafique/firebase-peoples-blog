0d09ecb65007f392a9eaa85ab728ad25af955de8
hassaan-shafique <hassaanshafique398@gmail.com>
2024-07-25 23:30:08 +0500

    create blog heading

--- src/component/CreateBlog.js
+++ src/component/CreateBlog.js
@@ -43,7 +43,7 @@ const CreateBlog = () => {
 
   return (
     <>
-      <h1>Create Blog</h1>
+      <h1>Share Your Thoughts with People's Blog</h1>
 
       <div className="blog-main">
         <div className="main-page">
